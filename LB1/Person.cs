d70cc5e94f02c3e42ccef879aac10d21705eb40e
AlexeyDesyatov <avd55@tpu.ru>
2025-12-03 21:24:17 +0700

    feature(LB1): добавлен класс Person для хранения персональных данных

---
+++ LB1/Person.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB1
+{
+    /// <summary>
+    /// Хранение и обработка персональных данных
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// Варианты пола
+        /// </summary>
+        public enum Gender
+        {
+            Male,
+            Female,
+            NonBinary
+        }
+
+        private string _name;
+        private string _surname;
+        private int _age;
+        private Gender _gender;
+
+        /// <summary>
+        /// Инициализируем новый класс
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        /// <param name="age"></param>
+        /// <param name="gendr"></param>
+        public Person(string name, string surname, int age, Gender gender)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Gendr = gender; 
+        }
+
+        /// <summary>
+        /// Получение и валидация имени
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Получение и валидация фамилии
+        /// </summary>
+        public string Surname
+        {
+            get { return _surname; }
+            set
+            {
+                _surname = value;
+            }
+        }
+
+        /// <summary>
+        /// Получение и валидация возраста
+        /// </summary>
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+            }
+        }
+
+        /// <summary>
+        /// Получение и валидация пола
+        /// </summary>
+        public Gender Gendr
+        {
+            get { return _gender; }
+            set
+            {
+                _gender = value;
+            }
+        }
+
+    }
+
+}
