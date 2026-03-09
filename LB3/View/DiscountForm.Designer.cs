44e20a73833da753824dd7949108ca0e73d8d742
AlexeyDesyatov <avd55@tpu.ru>
2026-03-09 21:19:20 +0700

    fix

--- LB3/View/DiscountForm.Designer.cs
+++ LB3/View/DiscountForm.Designer.cs
@@ -28,93 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBoxDiscounts = new System.Windows.Forms.GroupBox();
-            this.dataGridViewDiscounts = new System.Windows.Forms.DataGridView();
-            this.panelButtons = new System.Windows.Forms.Panel();
-            this.AddDiscount = new System.Windows.Forms.Button();
-            this.RemoveDiscount = new System.Windows.Forms.Button();
-            this.groupBoxDiscounts.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDiscounts)).BeginInit();
-            this.panelButtons.SuspendLayout();
-            this.SuspendLayout();
+            groupBoxDiscounts = new GroupBox();
+            dataGridViewDiscounts = new DataGridView();
+            panelButtons = new Panel();
+            groupBoxDiscounts.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewDiscounts).BeginInit();
+            SuspendLayout();
             // 
             // groupBoxDiscounts
             // 
-            this.groupBoxDiscounts.Controls.Add(this.dataGridViewDiscounts);
-            this.groupBoxDiscounts.Dock = System.Windows.Forms.DockStyle.Top;
-            this.groupBoxDiscounts.Location = new System.Drawing.Point(0, 0);
-            this.groupBoxDiscounts.Name = "groupBoxDiscounts";
-            this.groupBoxDiscounts.Size = new System.Drawing.Size(800, 100);
-            this.groupBoxDiscounts.TabIndex = 0;
-            this.groupBoxDiscounts.TabStop = false;
-            this.groupBoxDiscounts.Text = "Список скидок";
-            this.groupBoxDiscounts.Enter += new System.EventHandler(this.groupBox1_Enter);
+            groupBoxDiscounts.Controls.Add(dataGridViewDiscounts);
+            groupBoxDiscounts.Dock = DockStyle.Top;
+            groupBoxDiscounts.Location = new Point(0, 0);
+            groupBoxDiscounts.Name = "groupBoxDiscounts";
+            groupBoxDiscounts.Size = new Size(800, 200);
+            groupBoxDiscounts.TabIndex = 0;
+            groupBoxDiscounts.TabStop = false;
+            groupBoxDiscounts.Text = "Список скидок";
             // 
             // dataGridViewDiscounts
             // 
-            this.dataGridViewDiscounts.AllowUserToAddRows = false;
-            this.dataGridViewDiscounts.AllowUserToDeleteRows = false;
-            this.dataGridViewDiscounts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewDiscounts.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridViewDiscounts.Location = new System.Drawing.Point(3, 18);
-            this.dataGridViewDiscounts.Name = "dataGridViewDiscounts";
-            this.dataGridViewDiscounts.ReadOnly = true;
-            this.dataGridViewDiscounts.RowHeadersWidth = 51;
-            this.dataGridViewDiscounts.RowTemplate.Height = 24;
-            this.dataGridViewDiscounts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridViewDiscounts.Size = new System.Drawing.Size(794, 79);
-            this.dataGridViewDiscounts.TabIndex = 0;
+            dataGridViewDiscounts.AllowUserToAddRows = false;
+            dataGridViewDiscounts.AllowUserToDeleteRows = false;
+            dataGridViewDiscounts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewDiscounts.Dock = DockStyle.Fill;
+            dataGridViewDiscounts.Location = new Point(3, 23);
+            dataGridViewDiscounts.Name = "dataGridViewDiscounts";
+            dataGridViewDiscounts.ReadOnly = true;
+            dataGridViewDiscounts.RowHeadersWidth = 51;
+            dataGridViewDiscounts.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewDiscounts.Size = new Size(794, 174);
+            dataGridViewDiscounts.TabIndex = 0;
             // 
             // panelButtons
             // 
-            this.panelButtons.Controls.Add(this.RemoveDiscount);
-            this.panelButtons.Controls.Add(this.AddDiscount);
-            this.panelButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelButtons.Location = new System.Drawing.Point(0, 350);
-            this.panelButtons.Name = "panelButtons";
-            this.panelButtons.Size = new System.Drawing.Size(800, 100);
-            this.panelButtons.TabIndex = 1;
-            // 
-            // AddDiscount
-            // 
-            this.AddDiscount.Location = new System.Drawing.Point(75, 35);
-            this.AddDiscount.Name = "AddDiscount";
-            this.AddDiscount.Size = new System.Drawing.Size(150, 30);
-            this.AddDiscount.TabIndex = 0;
-            this.AddDiscount.Text = "Добавить скидку";
-            this.AddDiscount.UseVisualStyleBackColor = true;
-            // 
-            // RemoveDiscount
-            // 
-            this.RemoveDiscount.Location = new System.Drawing.Point(490, 35);
-            this.RemoveDiscount.Name = "RemoveDiscount";
-            this.RemoveDiscount.Size = new System.Drawing.Size(150, 30);
-            this.RemoveDiscount.TabIndex = 1;
-            this.RemoveDiscount.Text = "Удалить скидку";
-            this.RemoveDiscount.UseVisualStyleBackColor = true;
+            panelButtons.Location = new Point(272, 313);
+            panelButtons.Name = "panelButtons";
+            panelButtons.Size = new Size(250, 125);
+            panelButtons.TabIndex = 1;
             // 
             // DiscountForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panelButtons);
-            this.Controls.Add(this.groupBoxDiscounts);
-            this.Name = "DiscountForm";
-            this.Text = "DiscountForm";
-            this.groupBoxDiscounts.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDiscounts)).EndInit();
-            this.panelButtons.ResumeLayout(false);
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panelButtons);
+            Controls.Add(groupBoxDiscounts);
+            Name = "DiscountForm";
+            Text = "DiscountForm";
+            groupBoxDiscounts.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewDiscounts).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBoxDiscounts;
-        private System.Windows.Forms.DataGridView dataGridViewDiscounts;
-        private System.Windows.Forms.Panel panelButtons;
-        private System.Windows.Forms.Button RemoveDiscount;
-        private System.Windows.Forms.Button AddDiscount;
+        private GroupBox groupBoxDiscounts;
+        private DataGridView dataGridViewDiscounts;
+        private Panel panelButtons;
     }
 }
--- LB3/View/DiscountForm.cs
+++ LB3/View/DiscountForm.cs
@@ -7,19 +7,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System;
+using System.Windows.Forms;
+using Var5;  
 
 namespace View
 {
     public partial class DiscountForm : Form
     {
+        // 🔥 ПОЛЕ ДЛЯ ХРАНЕНИЯ СПИСКА СКИДОК
+        private List<IDiscount> _discounts = new List<IDiscount>();
+
         public DiscountForm()
         {
             InitializeComponent();
+            SetupDataGridView();  // Этот метод создадим ниже
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
+        // Метод для настройки DataGridView
+        private void SetupDataGridView()
         {
+            dataGridViewDiscounts.AutoGenerateColumns = false;
+            dataGridViewDiscounts.Columns.Clear();
 
+            // Добавляем колонки
+            dataGridViewDiscounts.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "Name",
+                HeaderText = "Название скидки",
+                DataPropertyName = "Name",
+                Width = 200
+            });
+
+            dataGridViewDiscounts.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "DiscountType",
+                HeaderText = "Тип скидки",
+                DataPropertyName = "DiscountType",
+                Width = 150
+            });
+
+            dataGridViewDiscounts.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "OriginPrice",
+                HeaderText = "Исходная цена",
+                DataPropertyName = "OriginPrice",
+                Width = 120
+            });
+
+            dataGridViewDiscounts.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "DiscountValue",
+                HeaderText = "Размер скидки",
+                DataPropertyName = "DiscountValue",
+                Width = 120
+            });
+
+            dataGridViewDiscounts.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "DiscountPrice",
+                HeaderText = "Итоговая цена",
+                DataPropertyName = "DiscountPrice",
+                Width = 120
+            });
         }
     }
 }
